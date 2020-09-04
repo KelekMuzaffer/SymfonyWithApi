4e6da0bf92ca0a61e5b5c18a55fbe92b3877f130
KelekMuzaffer <Muzaffer.kelek@le-campus-numerique.fr>
2020-09-04 17:32:20 +0200

    Page d'accueil et page pour un article en particulier : ok, reste à faire : ajout d'article, edit article, authentification: login, logout + liez les articles aux user avec le token dans le cookie

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+// Controller et template pages.html.twig creer grace à la commande : php bin/console make:controller dans le terminal
+class HomeController extends AbstractController
+{
+
+    /**
+     * Présente tous les articles dans la pages
+     * @Route(path="/", name="home")
+     */
+    public function home(HttpClientInterface $httpClient,Request $request)
+    {
+        $article = $httpClient->request('GET','https://localhost:8001/api/articles');
+
+        return $this->render('pages/home.html.twig', [
+            'controller_name' => 'HomeController',
+            'article' => $article->toArray()
+        ]);
+    }
+
+    /**
+     * @Route(path="/show/{id}", name="article_show")
+     */
+    public function show(HttpClientInterface $httpClient,$id)
+    {
+      $response = $httpClient->request('GET','https://localhost:8001/api/articles/'.$id);
+
+
+       return $this->render('pages/show.html.twig',[
+           'articleById' => $response->toArray()
+       ]);
+    }
+}
