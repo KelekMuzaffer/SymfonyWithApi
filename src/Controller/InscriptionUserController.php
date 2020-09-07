ad125103c51d94ad4a6a5204f00a9b915f34f770
KelekMuzaffer <Muzaffer.kelek@le-campus-numerique.fr>
2020-09-07 17:34:44 +0200

    Inscription et erreur inscription : ok

--- src/Controller/InscriptionUserController.php
+++ src/Controller/InscriptionUserController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Form\RegistrationUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Twig\Error\Error;
 
 class InscriptionUserController extends AbstractController
 {
@@ -13,14 +17,45 @@ class InscriptionUserController extends AbstractController
     /**
      * @Route("/inscription", name="InscriptionUser")
      */
-    public function registration()
+    public function registration(Request $request, HttpClientInterface $httpClient)
     {
         $form = $this->createForm(RegistrationUserType::class);
+        $form->handleRequest($request);
 
-        return $this->render('inscription_user/registrationUser.html.twig',[
+        $error = null;
+                if ($form->isSubmitted() && $form->isValid()) {
+
+        //            $response = $httpClient->request('GET', 'https://localhost:8001/api/users');
+        //            $info = $response->toArray();
+        //
+        //            foreach ($info['hydra:member'] as $value) {
+        //
+        //                if ($value['email'] === $_POST['registration_user']['email']) {
+        //                    $error = "L'email existe déjà, veuillez en utilisé un autre!";
+        //                    return $this->render('inscription_user/registrationUser.html.twig', [
+        //                        'form' => $form->createView(),
+        //                        'error' => $error
+        //                    ]);
+        //                }
+        //            }
+                    // Function qui permet d'enregistrer une inscription si il y à aucune erreur et catch une erreur si c'est pas bon
+                    try {
+                        $httpClient->request('POST', 'https://localhost:8001/api/users', [
+                            'json' => ['email' => $_POST['registration_user']['email'], 'password' => $_POST['registration_user']['password']['first'], 'confirmPassword' => $_POST['registration_user']['password']['second']]
+                        ]);
+                       return $this->redirectToRoute('home');
+                    } catch (\Throwable $e) {
+                        return $this->render('inscription_user/registrationUser.html.twig', [
+                                'form' => $form->createView(),
+                                'error' => $e->getMessage()
+                            ]);
+                    }
+                }
+                else {
+                    $form->getErrors(true);
+                }
+        return $this->render('inscription_user/registrationUser.html.twig', [
             'form' => $form->createView()
         ]);
-
     }
-
 }
--- src/Form/RegistrationUserType.php
+++ src/Form/RegistrationUserType.php
@@ -3,8 +3,15 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationUserType extends AbstractType
 {
@@ -12,9 +19,31 @@ class RegistrationUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('confirmPassword')
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => ['maxlength' => 50, 'class' => 'big-text'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrez un email',
+                    ]),
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'required' => true,
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe *', 'attr' => ['class' => 'big-text', 'minlength' =>  8, 'maxlength' => 20, 'placeholder' => '']],
+                'second_options' => ['label' => 'Répétez le mot de passe *', 'attr' => ['class' => 'big-text', 'minlength' => 8, 'maxlength' => 20, 'placeholder' => '']],
+                'invalid_message' => 'Le mot de passe ne correspond pas',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe valide']),
+                    new Length(['min' => 8,
+                                'minMessage' => 'Votre mot de passe doit contenir au moins 8 caractères',
+                                'max' => 20
+                    ]),
+                ],
+                'attr' => ['autocomplete' => 'off']
+            ])
+            ->add('Inscription', SubmitType::class)
         ;
     }
 
